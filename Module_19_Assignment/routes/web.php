9b5dd57c5e8c6a9c8222b8e776d2789aacf2c4cd
Sabbir-Hasan303 <habid9138@gmail.com>
2023-07-14 18:58:14 +0600

    test

--- Module_19_Assignment/routes/web.php
+++ Module_19_Assignment/routes/web.php
@@ -5,16 +5,7 @@ use App\Http\Controllers\Homecontroller;
 use App\Http\Controllers\PostDetailsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', [Homecontroller::class, 'page']);
 Route::get('/postDetails/{id}', [PostDetailsController::class, 'page']);
